57185d8a2451f9dab8e6594555190d9afccebbfb
Andrea Castro <ap.castrocastellon@gmail.com>
2020-01-17 17:08:05 -0300

    training pages

--- basics/10.variableScopep2.js
+++ basics/10.variableScopep2.js
@@ -7,32 +7,12 @@
 // let name = 'Andrea'
 
 if (true) {
-    // let name = 'Pavlina' //it's a parenting road!!!
+    name = 'Pavlina' // it's a parenting road!!!
     if (true) {
-        name = 'zwiva!' //assigning a new value!, the variable was never explicitly defined!!!!!!
+        name = 'zwiva!' // its a global var
         console.log(name)
     }
 } 
-
-if (true){
-    console.log(name)
-
-}
-// zwiva!
-// zwiva!
-
-
-
-// let name = 'Andrea'
-
-if (true) {
-    // let name = 'Pavlina' //it's a parenting road!!!
-    if (true) {
-        let name = 'zwiva!' //right way, but for the upper example it will work, but must show error
-        console.log(name)
-    }
-} 
-
 if (true){
     console.log(name)
 
--- basics/2.numbers.js
+++ basics/2.numbers.js
@@ -16,6 +16,6 @@
 
 let studentScore = 41
 let maxScore = 100
-let percent = (studentScore/maxScore)*100
+let percent = (studentScore / maxScore)*100
 
 console.log(percent)

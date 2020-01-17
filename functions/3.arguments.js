958a4ae14f0dba08b7a6c4f07dee25028d22f9ac
Andrea Castro <ap.castrocastellon@gmail.com>
2020-01-17 17:11:33 -0300

    day 3 reprocess

--- functions/3.arguments.js
+++ functions/3.arguments.js
@@ -1,11 +1,31 @@
-// multiple arguments
+// undefined argument
+let addUndef = function () {  
+}
+let resultUndef = addUndef()
+console.log(resultUndef)
 
+// multiple arguments
 let add = function(a, b, c){
     return a + b + c
 }
-
 let result = add(10, 1, 1)
 console.log(result)
 
-// default arguments
+// Default arguments
 
+let getScoreText = function(name, score=0){
+    return 'Name:' + name + '- Score: ' + score
+}
+getScoreText()
+let scoreText = getScoreText('Holo')
+console.log(scoreText)
+
+// Challenge area
+// total, tipPercent
+
+let getTip = function (total, tipPercent = .2){
+    return total * tipPercent
+}
+
+let tip = getTip(100, .25)
+console.log(tip)
